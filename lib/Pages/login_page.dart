16cbc17ac36db70bc3d6f4039b7d64c9d893d094
CBIPatel-Git <chintanitaliya0010@gmail.com>
2021-09-21 13:37:58 +0530

    Flutter Demo App Commit

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,66 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course_practise/utils/myrouts.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    int button_count = 0;
-
     return Material(
         color: Colors.white,
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/images/login_image.png',
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              'Welcome',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/images/login_image.png',
+                fit: BoxFit.cover,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'Enter UserName',
-                      labelText: 'Username',
-                    ),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: 'Enter Password',
-                      labelText: 'Password',
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      button_count++;
-                      print('Button pressed $button_count times');
-                    },
-                    child: Text(
-                      'Login',
-                    ),
-                  ),
-                ],
+              SizedBox(
+                height: 20,
               ),
-            )
-          ],
+              Text(
+                'Welcome',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter UserName',
+                        labelText: 'Username',
+                      ),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: 'Enter Password',
+                        labelText: 'Password',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                      child: Text(
+                        'Login',
+                      ),
+                      style: TextButton.styleFrom(
+                          minimumSize: const Size(120, 40)),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course_practise/utils/myrouts.dart';
 import 'Pages/home_page.dart';
 import 'Pages/login_page.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -19,8 +20,8 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(brightness: Brightness.dark),
       routes: {
         '/': (context) => LoginPage(),
-        '/home': (context) => HomePage(),
-        '/login': (context) => LoginPage()
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage()
       },
     );
   }
